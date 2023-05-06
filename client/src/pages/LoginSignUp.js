38da1da70fbf518771a6ad2e6e30bc896af5e0ad
stephen rogan <srogan26@gmail.com>
2023-05-06 13:26:31 -0500

    Have SignUp and Dashboard automatically navigating to the appropriate page based on whether there is an unexpired token in storage. No user Validation added yet

--- client/src/pages/LoginSignUp.js
+++ client/src/pages/LoginSignUp.js
@@ -1,8 +1,17 @@
 import Grid from '@mui/material/Grid';
 import Login from "../components/Login"
 import SignUp from "../components/SignUp"
+import { useState, useEffect } from 'react';
+import { UseIsLoggedIn } from "../utils/authenticate";
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginSignUp () {
+    const [userLogged, setUserLogged] = useState(UseIsLoggedIn())
+    
+  const navTo = useNavigate()
+  useEffect(()=>{
+    if(userLogged) navTo('/dash')
+  },[userLogged])
     const styles = {
         display: "flex",
         alignItems: 'center',
@@ -10,13 +19,13 @@ export default function LoginSignUp () {
     return (
         <Grid container style={styles}>
             <Grid item xs={12} md={5}>
-            <Login />
+            <Login setUserLogged={setUserLogged}/>
             </Grid>
             <Grid item xs={12} md={2} textAlign='center'>
             OR
             </Grid>
             <Grid item xs={12} md={5}>
-            <SignUp />
+            <SignUp setUserLogged={setUserLogged}/>
             </Grid>
         </Grid>
     )
--- client/src/utils/authenticate.js
+++ client/src/utils/authenticate.js
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
+import jwt_decode from "jwt-decode";
+
+export const UseLogIn = (data) => {
+  const token = data.addUser.authToken;
+  localStorage.setItem("auth_token", token);
+};
+
+export const UseIsLoggedIn = () => {
+    console.log(localStorage.getItem("auth_token"))
+const wasLogged = localStorage.getItem("auth_token") != '' ? true : false;
+  const [isLoggedIn, setIsLoggedIn] = useState(wasLogged);
+
+  useEffect(() => {
+    const userToken = localStorage.getItem("auth_token");
+    if (userToken) {
+      const user = jwt_decode(userToken);
+      if (user.exp > Date.now() && wasLogged ) {
+        setIsLoggedIn(false);
+        localStorage.setItem("auth_token", null);
+      }
+    } else if (wasLogged && isLoggedIn) setIsLoggedIn(false);
+  }, []);
+  console.log(isLoggedIn)
+  return isLoggedIn;
+};
 
 const Auth = {};
 
 Auth.logIn = (data) => {
-    const token = data.addUser.authToken
-    localStorage.setItem("auth_token", token)
-}
-
-export default Auth;
+  const token = data.addUser.authToken;
+  localStorage.setItem("auth_token", token);
+};
