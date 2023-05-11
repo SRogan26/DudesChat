febcadaf832d02f37601703cf6fd26df62c921f8
stephen rogan <srogan26@gmail.com>
2023-05-10 19:36:50 -0500

    Log in for existing users added. Frontend and backend gql mutations and login component functionality on client.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -4,11 +4,24 @@ export const ADD_USER = gql`
   mutation Mutation($username: String!, $email: String!, $password: String!, $firstName: String, $lastName: String) {
     addUser(username: $username, email: $email, password: $password, firstName: $firstName, lastName: $lastName) {
       authToken
-      newUser {
+      user {
         _id
         email
         username
       }
     }
   } 
+`;
+
+export const USER_LOGIN = gql`
+  mutation Mutation($email: String!, $password: String!) {
+    userLogIn(email: $email, password: $password) {
+      authToken
+      user {
+        _id
+        email
+        username
+      }
+    }
+  }
 `;
